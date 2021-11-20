c98735177596da3cd2fe1a8731dda1bd66952eba
Brian Peterson <brian@epetersons.com>
2021-11-20 15:08:35 -0600

    tuned yaw damper

--- p51YawDamper.lua
+++ p51YawDamper.lua
@@ -1,15 +1,18 @@
 TARGET_AIRCRAFT = "51D"
 THIS_AIRCRAFT = hub.getSimString("MetadataStart/_ACFT_NAME")
 
+COMFORT_ZONE_PCT = .1
+
 RUDDER = {}
 RUDDER.TRIM_CTRL = "RUDDER_TRIM"
 RUDDER.TRIM_VAL = "P-51D/RUDDER_TRIM"
 RUDDER.BALL = "P-51D/SLIPBALL"
 RUDDER.CENTER = 65536 / 2
-RUDDER.TRIM_INCR = 100 -- 3200 = 1 degree
+RUDDER.CENTER_LEFT = RUDDER.CENTER * (1 - COMFORT_ZONE_PCT)
+RUDDER.CENTER_RIGHT = RUDDER.CENTER * (1 + COMFORT_ZONE_PCT)
+RUDDER.TRIM_INCR = 500 -- 3200 = 1 degree
 MESSAGE = "NONE"
 
-COMFORT_ZONE_PCT = .01
 
 hub.registerOutputCallback(function()
     if string.find(THIS_AIRCRAFT, TARGET_AIRCRAFT) then
@@ -17,11 +20,11 @@ hub.registerOutputCallback(function()
         ball = tonumber(hub.getSimInteger(RUDDER.BALL))
         msg = "center= " .. RUDDER.CENTER .. ", current trim: " .. trim .. ", ball: " .. ball
         trim_adjust = 0
-        if ball < RUDDER.CENTER then
+        if ball < RUDDER.CENTER_LEFT then
             -- ball is left of center. step on the ball, adding left rudder trim
             msg = msg .. ", ball is left, sub trim:" .. RUDDER.TRIM_INCR
             trim_adjust = trim_adjust - RUDDER.TRIM_INCR
-        elseif ball > RUDDER.CENTER then
+        elseif ball > RUDDER.CENTER_RIGHT then
             -- ball is right of center. step on the ball, adding left rudder trim
             msg = msg .. ", ball is right, add trim: " .. RUDDER.TRIM_INCR
             trim_adjust = trim_adjust + RUDDER.TRIM_INCR
